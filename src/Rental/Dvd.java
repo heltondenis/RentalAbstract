f0bb611df39fb543f916d8619dfc43f2f879e3c9
Helton Denis <heltondeniss@gmail.com>
2018-08-26 11:57:53 -0300

    class dvd extends a itemAbstract

---
+++ src/Rental/Dvd.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rental;
+
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author heltondenis
+ */
+
+public class Dvd extends ItemAbstract {
+    
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    //constuctor patters
+    public Dvd() {
+        
+    }
+
+    @Override
+    public void register() {
+        
+    }
+
+    @Override
+    public void lend() {
+        
+    }
+
+    @Override
+    public void return_item() {
+       
+    }
+    
+}
